66b761e0a10695658e76021127780a854b0d19b0
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-15 00:15:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,9 +72,8 @@ class Dog
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
-
     end
-
+    dog
   end
 
   # def self.find_by_name(name)
